42fa286ba117b01cb3b9a3f12d54fa241b3ee8da
srinibas khuntia <srinibas.k@pillir.io>
2023-01-28 20:27:06 +0530

    C++ intro

---
+++ p2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << "Enter a number" << endl;
+    // int a;
+    // cin >> a;
+    // cout << "User entered " << a << endl;
+
+    // integer
+    // int n = 5;
+    // cout << "int " << n << endl;
+
+    // char ch = 'a';
+    // cout << "char " << ch << endl;
+
+    // bool flag = true;
+    // cout << "flag " << flag << endl;
+
+    // sizeof
+    // bool flag = true;
+    // cout << "size of flag " << flag << endl;
+}
